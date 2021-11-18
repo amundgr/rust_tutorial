205fe658992078ccb727ef6af3a3fc23de3855c7
Amund Grorud <amund@sqhead.com>
2021-11-18 20:15:27 +0100

    current progress

--- chapter_13/closure/src/main.rs
+++ chapter_13/closure/src/main.rs
@@ -15,20 +15,43 @@ where
     value: Option<u32>,
 }
 
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = |num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
     if intensity < 25 {
         println!(
             "Today, to {} pushups!",
-            expensive_result(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_result(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -36,7 +59,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_result(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
